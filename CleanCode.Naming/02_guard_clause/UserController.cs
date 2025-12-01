47537306889b85601e06174bd0eb30082f614085
ssuarezs <suarez.santiago.113@gmail.com>
2025-11-30 21:28:43 -0500

    Extract common classes in Naming Library

--- CleanCode.Naming/02_guard_clause/UserController.cs
+++ CleanCode.Naming/02_guard_clause/UserController.cs
@@ -1,35 +1,5 @@
-﻿using System.Text.Json;
-
+﻿
 namespace CleanCode.Naming._02_guard_clause;
-
-public sealed record UserRequest(string? Name, string? Surname, string? Email, string? Password, string? PasswordConfirmation);
-
-public sealed record Response(int StatusCode, string Body);
-
-public class User(int? id, string name, string surname, string email, string hashedPassword)
-{
-    public int? Id { get; set; } = id;
-    public string Name { get; } = name;
-    public string Surname { get; } = surname;
-    public string Email { get; } = email;
-    public string HashedPassword { get; } = hashedPassword;
-
-    public string ToJson()
-    {
-        return JsonSerializer.Serialize(this);
-    }
-}
-
-public interface IPasswordHasher
-{
-    string Hash(string password);
-}
-
-public interface IUserRepository
-{
-    User? ByEmail(string email);
-    int Save(User user);
-}
 
 public class UsersController(IUserRepository userRepository, IPasswordHasher passwordHasher)
 {
